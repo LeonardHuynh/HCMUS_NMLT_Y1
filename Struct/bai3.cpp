f5fc3c7d1a7de0b30da866bec08d0a17502bcbee
Nguyễn Hải Tuyên <88772805+HaiTuyen@users.noreply.github.com>
2021-12-11 20:52:21 +0700

    Update bai3.cpp

--- Struct/bai3.cpp
+++ Struct/bai3.cpp
@@ -73,37 +73,37 @@ void xuat_vitri(DIEM2D A, DIEM2D B, DIEM2D C, DIEM2D D) {
 
 int main() 
 {
-			printf("Bai 3:\n");
-			printf("a.Nhap, xuat diem 2D:\n");
-			DIEM2D p1;
-			nhap_diem2D(p1);
-			printf("Diem da nhap: ");
-			xuat_diem2D(p1);
+	printf("Bai 3:\n");
+	printf("a.Nhap, xuat diem 2D:\n");
+	DIEM2D p1;
+	nhap_diem2D(p1);
+	printf("Diem da nhap: ");
+	xuat_diem2D(p1);
 
-			printf("\nb.Tinh khoang cach giua 2 diem A, B:\n");
-			DIEM2D A1, B1;
-			printf("Nhap diem A:\n");
-			nhap_diem2D(A1);
-			printf("Nhap diem B:\n");
-			nhap_diem2D(B1);
-			double khoangcachAB = Tinh_KhoangCach(A1, B1);
-			printf("Khoang cach giua 2 diem A,B: %lf", khoangcachAB);
+	printf("\nb.Tinh khoang cach giua 2 diem A, B:\n");
+	DIEM2D A1, B1;
+	printf("Nhap diem A:\n");
+	nhap_diem2D(A1);
+	printf("Nhap diem B:\n");
+	nhap_diem2D(B1);
+	double khoangcachAB = Tinh_KhoangCach(A1, B1);
+	printf("Khoang cach giua 2 diem A,B: %lf", khoangcachAB);
 
-			printf("\nc.Tinh chu vi va dien tich tam giac tu 3 diem A, B, C:\n");
-			DIEM2D A, B, C;
-			printf("Nhap diem A:\n");
-			nhap_diem2D(A);
-			printf("Nhap diem B:\n");
-			nhap_diem2D(B);
-			printf("Nhap diem C:\n");
-			nhap_diem2D(C);
-			printf("Chu vi tam giac ABC: %lf\n", Tinh_ChuVi(A, B, C));
-			printf("Dien tich tam giac ABC: %lf", Tinh_DienTich(A, B, C));
+	printf("\nc.Tinh chu vi va dien tich tam giac tu 3 diem A, B, C:\n");
+	DIEM2D A, B, C;
+	printf("Nhap diem A:\n");
+	nhap_diem2D(A);
+	printf("Nhap diem B:\n");
+	nhap_diem2D(B);
+	printf("Nhap diem C:\n");
+	nhap_diem2D(C);
+	printf("Chu vi tam giac ABC: %lf\n", Tinh_ChuVi(A, B, C));
+	printf("Dien tich tam giac ABC: %lf", Tinh_DienTich(A, B, C));
 
-			printf("\nd.Kiem tra diem D nam trong tam giac ABC:\n");
-			DIEM2D D;
-			printf("Nhap diem D:\n");
-			nhap_diem2D(D);
-			xuat_vitri(A, B, C, D);
-			return 0;
+	printf("\nd.Kiem tra diem D nam trong tam giac ABC:\n");
+	DIEM2D D;
+	printf("Nhap diem D:\n");
+	nhap_diem2D(D);
+	xuat_vitri(A, B, C, D);
+	return 0;
 }
