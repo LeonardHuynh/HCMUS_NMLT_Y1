a6d3ea5128852f738630c2b557298ee18b16a960
Nguyễn Hải Tuyên <88772805+HaiTuyen@users.noreply.github.com>
2021-12-12 08:16:42 +0700

    Create bai2.cpp
    Bài 2. Đếm số lượng số nguyên tố trong ma trận

---
+++ Array2D/bai2.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define MAX_COLS 10
+
+void NhapMang2D(int a[][MAX_COLS], int& rows, int& cols)
+{
+	printf("Nhap so dong: ");
+	scanf_s("%d", &rows);
+	printf("Nhap so cot: ");
+	scanf_s("%d", &cols);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("Nhap a[%d][%d]: ", i, j);
+			scanf_s("%d", &a[i][j]);
+		}
+	}
+}
+
+void XuatMang2D(int a[][MAX_COLS], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int KTra_SoNguyenTo(int n)
+{
+	if (n < 2)
+	{
+		return 0;
+	}
+	else if (n == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		for (int i = 2; i <= n / 2; i++)
+		{
+			if (n % i == 0)
+			{
+				return 0;
+			}
+		}
+		return 1;
+	}
+}
+
+int Dem_SoNguyenTo(int a[][MAX_COLS], int rows, int cols)
+{
+	int dem = 0;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (KTra_SoNguyenTo(a[i][j]) == 1)
+				dem++;
+		}
+	}
+	return dem;
+}
