1f54b62710c581765cd39ea062a8e46371091c40
Nguyễn Hải Tuyên <88772805+HaiTuyen@users.noreply.github.com>
2021-12-12 08:24:17 +0700

    Create bai6.cpp
    Bài 6. Liệt kê các dòng chứa toàn số chẵn trong ma trận.

---
+++ Array2D/bai6.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAX_COLS 10
+
+void NhapMang2D(int a[][MAX_COLS], int& rows, int& cols)
+{
+	printf("Nhap so dong: ");
+	scanf_s("%d", &rows);
+	printf("Nhap so cot: ");
+	scanf_s("%d", &cols);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("Nhap a[%d][%d]: ", i, j);
+			scanf_s("%d", &a[i][j]);
+		}
+	}
+}
+
+void XuatMang2D(int a[][MAX_COLS], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void LietKe_Dong_ToanSoChan(int a[][MAX_COLS], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int check = 1;
+		for (int j = 0; j < cols; j++)
+		{
+			if (a[i][j] % 2 != 0)
+			{
+				check = 0;
+				break;
+			}
+		}
+		if (check == 1)
+		{
+			printf("%d ", i + 1);
+		}
+	}
+}
+
+int main()
+{
+	int a[10][10];
+	int rows, cols;
+	NhapMang2D(a, rows, cols);
+	printf("Ma tran da nhap:\n");
+	XuatMang2D(a, rows, cols);
+	printf("Dong chua toan so chan trong ma tran: ");
+	LietKe_Dong_ToanSoChan(a, rows, cols);
+	return 0;
+}
